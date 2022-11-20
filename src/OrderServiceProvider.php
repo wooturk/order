c44b858d947ac327cb76c8232020d56fd0bc2615
tulparstudyo <teknovipyazilim@gmail.com>
2022-11-20 22:28:10 +0300

    update

--- src/OrderServiceProvider.php
+++ src/OrderServiceProvider.php
@@ -23,13 +23,13 @@ class OrderServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		Route::get('/user', [UserController::class, 'index']);
-		Route::post('/user', [UserController::class, 'post']);
-		Route::group(['middleware' => ['auth:sanctum']], function(){
-			Route::get('/users', [UserController::class, 'list']);
-			Route::get('/user/{id}', [UserController::class, 'get']);
-			Route::put('/user/{id}', [UserController::class, 'put']);
-			Route::delete('/user/{id}', [UserController::class, 'delete']);
+		Route::get('/order', [UserController::class, 'index'])->name('order-index');
+		Route::group(['middleware' => ['auth:sanctum','wooturk.gateway']], function(){
+			Route::post('/order', [UserController::class, 'post'])->name('order-create');
+			Route::get('/orders', [UserController::class, 'list'])->name('order-list');
+			Route::get('/order/{id}', [UserController::class, 'get'])->name('order-get');
+			Route::put('/order/{id}', [UserController::class, 'put'])->name('order-update');
+			Route::delete('/order/{id}', [UserController::class, 'delete'])->name('order-delete');
 		});
 	}
 }
